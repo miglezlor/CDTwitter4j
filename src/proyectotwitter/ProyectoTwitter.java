2e5a32381d0aad1087c06b4dc467e744c41361b4
mgonzalezlorenzo <mgonzalezlorenzo@atanasoff05.danielcastelao.org>
2016-04-11 09:07:04 +0200

    Initial

---
+++ src/proyectotwitter/ProyectoTwitter.java
@@ -0,0 +1,19 @@
+package proyectotwitter;
+
+import twitter4j.*;
+
+/**
+ *
+ * @author mgonzalezlorenzo
+ */
+public class ProyectoTwitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Twitter mitwitter = new TwitterFactory().getInstance();
+    }
+    
+}
